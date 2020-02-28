55bd1f3d9c9d34bc6b51147b823e418ce9d09038
devdaniyal <daniyalrazanoorani@gmail.com>
2020-02-28 11:30:02 +0500

    math expression

---
+++ Math expression Eliminating ambiguity/mathEliminating.js
@@ -0,0 +1,11 @@
+function helloWorld(){
+    var num = 1 + 3 * 4;
+    // we expecting answer 16 but its 13 becz javasript consider bodmas rule.
+    // console.log('num',num); 
+    var profit = 4 * (1 + 3); 
+    // it will answer 16
+    // console.log('profit',profit);
+    var result = (2 * 4) * (4 + 2);
+    // console.log("result", result);
+}
+helloWorld()
