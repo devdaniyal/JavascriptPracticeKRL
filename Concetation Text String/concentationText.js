b74d9487505f72b9ac3db0ff2acc636a49e95bfc
devdaniyal <daniyalrazanoorani@gmail.com>
2020-03-02 11:41:51 +0500

    concentationText

---
+++ Concetation Text String/concentationText.js
@@ -0,0 +1,11 @@
+function helloWorld(){
+    // var b = document.getElementById('valName').value;
+    // alert("My Name is "  + b);
+    // var name = 'Shayan '; 
+    // var surname = 'daniyal ';
+    // var expression = '!';
+    // it will show all 3 variable in alert..
+    // alert(name + surname + expression);
+    // var custom = name + surname + expression;
+    // alert(custom);
+}
