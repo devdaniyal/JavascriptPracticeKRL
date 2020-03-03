bdb2755fd5b5da85f0406aef5a6a96d85ddcbfe3
devdaniyal <daniyalrazanoorani@gmail.com>
2020-03-03 15:15:28 +0500

    promt and ifstatement

---
+++ Prompt/prompt.js
@@ -0,0 +1,22 @@
+function helloWorld(){
+    // var response = prompt('Whats your name?','daniyal')
+    // console.log(response,'response');
+    // document.write(response);
+
+    // var first = "Whats your name?";
+    // var response = prompt(first,'');
+    // console.log(response,'response');
+    // document.write(response);
+
+    // var question = "Whats your name?";
+    // var defaultAnswer = "daniyal";
+    // var response = prompt(question,defaultAnswer);
+    // console.log(response,'response');
+    // document.write(response);
+
+    // var numberOfCats = prompt("How many cats?",'');
+    // var tooManyCats = numberOfCats + 1;
+    // console.log(tooManyCats,'tooManyCats');
+    // document.write(tooManyCats);
+}
+helloWorld()
